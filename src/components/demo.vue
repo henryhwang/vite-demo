a7a62f8d3974bc5a9de83563a50a91cb4d42a407
Henry H Wang <phwhx@hotmail.com>
2021-11-12 11:50:19 +0800

    add .vue support

---
+++ src/components/demo.vue
@@ -0,0 +1,15 @@
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: 'Demo',
+    props: {},
+    setup() {
+        return () => {
+            return (
+                <div>Hello from Demo</div>
+            )
+        }
+    }
+})
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import {vue} from '@vite/plugin-vue'
 
 export default defineConfig({
+  plugins: [vue],
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
